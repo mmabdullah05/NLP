abea00383cac24b9109830663162e25e4ef1738d
mmabdullah05 <100152133+mmabdullah05@users.noreply.github.com>
2024-04-27 06:27:19 +0000

    initial python project testing layout

--- Hello.py
+++ Hello.py
@@ -1,3 +1,14 @@
-a = 3
-b= 2
-print(a+b)
+def more_hello():
+    return "hi"
+
+
+def more_goodbye():
+    return "bye"
+
+
+# Code to get warning
+# var = 1
+# var = var
+
+# Code to get error
+# foo =
